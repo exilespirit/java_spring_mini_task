a8ce5df954c6e8056e33a73916ac73af1a52f74e
exilespirit <exilespirit@gmail.com>
2022-11-16 20:11:38 +0500

    Swagger configuration moved to separate class

--- src/main/java/com/spirit/Application.java
+++ src/main/java/com/spirit/Application.java
@@ -9,16 +9,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public Docket productApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.spirit")).build();
-    }
+
 }
